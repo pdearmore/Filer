ae03ad443fd7895e419419b8d9e2c5b7e4f5204a
Phil Dearmore <philip@dearmore.net>
2017-06-11 09:08:53 -0400

    Ability to import simple module

---
+++ Filer/Public/Split-Folders.ps1
@@ -0,0 +1,38 @@
+
+# Split-Folders
+<#.Synopsis
+<!<SnippetShortDescription>!>
+.DESCRIPTION
+<!<SnippetLongDescription>!>
+.EXAMPLE
+<!<SnippetExample>!>
+.EXAMPLE
+<!<SnippetAnotherExample>!>
+#>
+function Split-Folders
+{
+    [CmdletBinding()]
+    param
+    (
+        # <!<SnippetParam1Help>!>
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=0)]
+        $parameter1,
+
+        # <!<SnippetParam2Help>!>
+        [int]$parameter2)
+
+        Begin
+        {
+          
+        }
+
+        Process
+        {
+          
+        }
+
+        End
+        {
+
+        }
+}
