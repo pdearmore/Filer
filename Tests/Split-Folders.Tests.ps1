a18ada201554b686197414c8e78300cefc2137dd
Phil Dearmore <philip@dearmore.net>
2017-06-15 21:32:50 -0400

    Started working on the tests a bit.

---
+++ Tests/Split-Folders.Tests.ps1
@@ -0,0 +1,10 @@
+$projectRoot = Resolve-Path "$PSScriptRoot\.."
+$moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psm1")
+$moduleName = Split-Path $moduleRoot -Leaf
+
+Describe "Split-Folders" {
+
+    It "Should create a bunch of folders" {
+    }
+
+}
